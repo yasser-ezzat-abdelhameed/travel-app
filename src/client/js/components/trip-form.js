c73db7b8071593bd0c6286c33e4df8d6c507e895
Yasser Ezzat <yasser.ezzat.abdelhamid@gmail.com>
2020-07-27 22:26:37 +0200

    update: Create Trip form
    - Base info, styles, and saving into the app state

--- src/client/js/components/trip-form.js
+++ src/client/js/components/trip-form.js
@@ -9,8 +9,29 @@ export default function renderTripForm() {
     <button id="back">Go back</button>
     <form name="trip-form" id="trip-form" onsubmit="return Client.submitTrip(event)">
       <section class="form-group">
-        <label for="destination">Destination</label>
+        <label for="destination">Place</label>
         <input type="text" id="destination" name="destination" />
+      </section>
+      <section class="form-group">
+        <label for="start-date">From</label>
+        <input type="date" id="start-date" name="start-date" />
+      </section>
+      <section class="form-group">
+        <label for="end-date">To</label>
+        <input type="date" id="end-date" name="end-date" />
+      </section>
+      <section class="form-group">
+        <label for="hotel">Hotel</label>
+        <input type="text" id="hotel" name="hotel" />
+      </section>
+      <section class="form-group">
+        <label for="todo-btn">Todo</label>
+        <button type="text" id="todo-btn" name="todo-btn">+ Add item</button>
+      </section>
+      <ul id="todo-list"></ul>
+      <section class="form-group">
+        <label for="notes">Notes</label>
+        <textarea rows="6" id="notes" name="notes"></textarea>
       </section>
       <input type="submit" value="Submit" />
     </form>
@@ -28,6 +49,28 @@ export default function renderTripForm() {
     store.dispatch({
       type: SET_CURRENT_VIEW,
       currentView: VIEWS.HOME,
+    });
+  });
+
+  document.querySelector("#todo-btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    const random = Date.now();
+    const id = `todo-item-${random}`;
+    const removeBtnId = `${id}-btn`;
+    const todoItemElm = document.createElement("li");
+    todoItemElm.innerHTML = `
+      <section class="form-group todo-form-group">
+        <input type="text" id="${id}" name="todo-item" />
+        <button id="${removeBtnId}">-</button>
+      </section>
+    `;
+    const todoListElm = document.querySelector("#todo-list");
+    todoListElm.appendChild(todoItemElm);
+
+    /* Event listeners */
+    document.querySelector(`#${removeBtnId}`).addEventListener("click", (event) => {
+      event.preventDefault();
+      todoListElm.removeChild(todoItemElm);
     });
   });
 }
--- src/client/js/eventHandlers/submitTrip.js
+++ src/client/js/eventHandlers/submitTrip.js
@@ -5,10 +5,25 @@ import VIEWS from "../views";
 export default function submitTrip(event) {
   event.preventDefault();
   const destination = document.querySelector("#destination").value;
+  const startDate = document.querySelector("#start-date").value;
+  const endDate = document.querySelector("#end-date").value;
+  const hotel = document.querySelector("#hotel").value;
+  const notes = document.querySelector("#notes").value;
+  const todoElements = document.querySelectorAll("[name=todo-item]");
+  const todoList = [];
+  for (let { value } of todoElements) {
+    todoList.push(value);
+  }
+
   store.dispatch({
     type: ADD_TRIP,
     trip: {
       destination,
+      startDate,
+      endDate,
+      hotel,
+      notes,
+      todoList,
     },
   });
   store.dispatch({
