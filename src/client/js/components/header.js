48a67905fd0c6a64c9f0094170835d77d8d517ca
yasser-ezzat-abdelhameed <yasseraudio@gmail.com>
2020-07-29 22:15:28 +0200

    update: Header updates

--- src/client/js/components/header.js
+++ src/client/js/components/header.js
@@ -3,7 +3,7 @@ import { SET_CURRENT_VIEW, REMOVE_SELECTED_TRIP_ID } from "../actions";
 import VIEWS from "../views";
 
 export default function renderHeader() {
-  const { currentView, selectedTripId } = store.getState();
+  const { currentView } = store.getState();
   const header = document.querySelector("#header");
   if (currentView === VIEWS.HOME) {
     header.innerHTML = `
@@ -13,7 +13,7 @@ export default function renderHeader() {
     header.innerHTML = `
       <div>
         <span class="back-btn"><i class="fa fa-arrow-left"></i></span>
-        <span>${selectedTripId ? "Edit" : "Create"} Trip</span>
+        <span>${renderHeaderText()}</span>
       </div>
     `;
     document.querySelector(".back-btn").addEventListener("click", (_) => {
@@ -25,5 +25,16 @@ export default function renderHeader() {
         type: REMOVE_SELECTED_TRIP_ID,
       });
     });
+  }
+}
+
+function renderHeaderText() {
+  const { currentView, selectedTripId } = store.getState();
+  if (currentView === VIEWS.TRIP_DETAILS) {
+    const { trips } = store.getState();
+    const { destination } = trips.find((item) => item.id === selectedTripId);
+    return destination;
+  } else {
+    return (selectedTripId ? "Edit" : "Create") + " Trip";
   }
 }
