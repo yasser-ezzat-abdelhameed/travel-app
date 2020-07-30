858d37a78ca530e7cd7b802b58fa4fc639d0ba2d
Yasser Ezzat <yasser.ezzat.abdelhamid@gmail.com>
2020-07-30 22:25:04 +0200

    update: Added service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -42,5 +43,6 @@ module.exports = {
       favicon: "./src/client/assets/favicon.ico",
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
